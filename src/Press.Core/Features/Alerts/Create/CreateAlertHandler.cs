9a36d567c385cbb3433a52f32a280248f7a10361
clapis <clapis@users.noreply.github.com>
2024-12-27 18:13:32 -0300

    feat: max number of alerts

--- src/Press.Core/Features/Alerts/Create/CreateAlertHandler.cs
+++ src/Press.Core/Features/Alerts/Create/CreateAlertHandler.cs
@@ -6,8 +6,19 @@ namespace Press.Core.Features.Alerts.Create;
 
 public class CreateAlertHandler(IAlertStore store) : IRequestHandler<CreateAlertRequest>
 {
+    private const int MaxNumberOfAlerts = 20;
     public async Task Handle(CreateAlertRequest request, CancellationToken cancellationToken)
     {
+        var alerts = await store.GetByUserIdAsync(request.UserId, cancellationToken);
+        
+        // Ensure max number of alerts is not exceeded
+        if (alerts.Count >= MaxNumberOfAlerts)
+            throw new Exception("User {UserId} has exceeded max number of alerts.");
+        
+        // If there is already an alert for this keyword, ignore request
+        if (alerts.Any(x => x.Term == request.Keyword)) 
+            return;
+        
         var alert = new Alert
         {
             Term = request.Keyword, 
